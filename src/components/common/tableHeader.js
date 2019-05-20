ca0386d8642544180c96a9764dff5298f3bd0ac1
AgfeV <elysiumzero117@gmail.com>
2019-05-20 15:21:59 -0700

    Further mix level abst fix with table class

--- src/components/common/tableHeader.js
+++ src/components/common/tableHeader.js
@@ -29,11 +29,11 @@ class TableHeader extends Component {
   }
   render(){
 
-    const {onSort, columns, sortColumn, onClick} = this.props;
+    const { columns} = this.props;
 
       return(<thead>
       <tr>
-        {this.props.columns.map(column => <th className="clickable" key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{column.label}{this.renderSortIcon(column)}</th>)}
+        {columns.map(column => <th className="clickable" key={column.path || column.key} onClick={() => this.raiseSort(column.path)}> {column.label} {this.renderSortIcon(column)}</th>)}
       </tr>
       </thead>
   )
--- src/components/moviesTable.js
+++ src/components/moviesTable.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import Like from "./common/like";
-import TableHeader from "./common/tableHeader";
-import TableBody from "./common/tableBody";
-
+import Table from "./common/table"
 class MoviesTable extends Component {
 //we are going to create an object to send down to the table header class with all the path and lable information
 columns = [
@@ -12,7 +10,7 @@ columns = [
   {path:'numberInStock', label: 'Stock'},
   {path:'dailyRentalRate', label: 'Rate'},
   //will represent the like and the delete button but dont have a label or path for sorting
-  //will pass the function by refrence with onlike and onDelete taht will take a movie and return a react fragment. 
+  //will pass the function by refrence with onlike and onDelete taht will take a movie and return a react fragment.
   {key:'like', content:  movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)}/>},
   {key:'delete', content: movie => <button onClick={() => this.props.onDelete(movie)} className="btn btn-danger btn-sm m-2">Delete</button> }
 
@@ -21,18 +19,7 @@ columns = [
   render() {
     const {movies, sortColumn, onSort} = this.props;
     return (
-      <table class="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-          />
-
-
-        <TableBody data={movies} columns={this.columns} />
-
-
-      </table>
+      <Table columns = {this.columns} data={movies} onSort={onSort} sortColumn={sortColumn}/>
     );}
 }
 export default MoviesTable;
